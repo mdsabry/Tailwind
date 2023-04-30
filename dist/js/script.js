e5fa08bce72e6e06cdccdc5431e1793aba4c8773
Mohamed Sabry <47719823+mdsabry@users.noreply.github.com>
2023-04-30 23:49:49 +0200

    Create WebSite With TailwindCss By Nour Homsi

---
+++ dist/js/script.js
@@ -0,0 +1,6 @@
+const mobileBtn = document.getElementById('mobileBtn')
+const mobileMenu = document.getElementById('mobileMenu')
+
+mobileBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden")
+})
